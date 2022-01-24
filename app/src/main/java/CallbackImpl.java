aa5812c85a0c8b862003427826959dec7f043349
aslbek1202 <aslbekazamatov5@gmail.com>
2022-01-24 21:23:17 +0500

    Movie catalogue part 1

---
+++ app/src/main/java/CallbackImpl.java
@@ -0,0 +1,4 @@
+import javax.security.auth.callback.Callback;
+
+public class CallbackImpl implements Callback {
+}
---
+++ app/src/main/java/com/devoloper/moviecatalog/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.devoloper.moviecatalog
+
+import android.os.Binder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.devoloper.moviecatalog.models.Movie
+import com.devoloper.moviecatalog.models.MovieResponse
+import com.devoloper.moviecatalog.services.MovieApiInterface
+import com.devoloper.moviecatalog.services.MovieApiService
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rv_movies_list.layoutManager = LinearLayoutManager(this)
+        rv_movies_list.setHasFixedSize(true)
+        getMovieData { movies: List<Movie> ->
+            rv_movies_list.adapter = MovieAdapter(movies)
+
+        }
+
+    }
+
+    private fun getMovieData(callback: (List<Movie>) -> Unit) {
+        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
+        apiService.getMovieList().enqueue(object : Callback<MovieResponse> {
+            override fun onResponse(
+                call: Call<MovieResponse>,
+                response: Response<MovieResponse>,
+            ) {
+
+                return callback(response.body()!!.movies)
+            }
+
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+
+            }
+
+        })
+
+    }
+
+
+}
